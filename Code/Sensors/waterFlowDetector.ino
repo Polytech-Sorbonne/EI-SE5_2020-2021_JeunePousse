5d35281e33b7d23436c6cf5fb02a78d882a3118f
walidghettas <59226046+walidghettas@users.noreply.github.com>
2021-02-11 17:45:59 +0100

    Update waterFlowDetector.ino

--- Code/Sensors/waterFlowDetector.ino
+++ Code/Sensors/waterFlowDetector.ino
@@ -1,34 +1,34 @@
-// reading liquid flow rate using Seeeduino and Water Flow Sensor from Seeedstudio.com
-// Code adapted by Charles Gantt from PC Fan RPM code written by Crenn @thebestcasescenario.com
-// http:/themakersworkbench.com http://thebestcasescenario.com https://www.seeedstudio.com
- 
-volatile int NbTopsFan; //measuring the rising edges of the signal
-int Calc;
-int hallsensor = 2;    //The pin location of the sensor
- 
-void rpm ()     //This is the function that the interupt calls
-{
-    NbTopsFan++;  //This function measures the rising and falling edge of the
- 
-    hall effect sensors signal
+//Branchement, rouge = VIN, noir = GND et jaune = D2
+
+/***************************************************
+ This example reads Water flow sensor Sensor.
+
+ Created 2016-3-13
+ By berinie Chen <bernie.chen@dfrobot.com>
+
+ GNU Lesser General Public License.
+ See <http://www.gnu.org/licenses/> for details.
+ All above must be included in any redistribution
+ ****************************************************/
+
+/***********Notice and Trouble shooting***************
+ 1.Connection and Diagram can be found here  https://www.dfrobot.com/wiki/index.php?title=Water_Flow_Sensor_-_1/8%22_SKU:_SEN0216
+ 2.This code is tested on Arduino Uno.
+ ****************************************************/
+volatile double waterFlow;
+void setup() {
+  Serial.begin(115200);  //baudrate
+  waterFlow = 0;
+  attachInterrupt(0, pulse, RISING);  //DIGITAL Pin 2: Interrupt 0
+}
+void loop() {
+  Serial.print("waterFlow:");
+  Serial.print(waterFlow);
+  Serial.println("   L");
+  delay(500);
 }
 
-void setup() 
+void pulse()   //measure the quantity of square wave
 {
-    pinMode(hallsensor, INPUT);
-    Serial.begin(9600); 
-    attachInterrupt(0, rpm, RISING);
-}
-
-void loop ()
-{
-    NbTopsFan = 0;   //Set NbTops to 0 ready for calculations
-    sei();      //Enables interrupts
-    delay (1000);   //Wait 1 second
-    cli();      //Disable interrupts
-    Calc = (NbTopsFan * 60 / 7.5); //(Pulse frequency x 60) / 7.5Q, = flow rate
- 
-    in L/hour
-    Serial.print (Calc, DEC); //Prints the number calculated above
-    Serial.print (" L/hour\r\n"); //Prints "L/hour" and returns a  new line
+  waterFlow += 1.0 / 5880.0;
 }
